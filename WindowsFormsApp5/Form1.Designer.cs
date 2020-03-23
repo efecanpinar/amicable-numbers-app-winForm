dd01d52aebf4c66e88436e8ec9ca9e762ff0178e
efecanpinar <efecanpinar44@gmail.com>
2020-03-23 22:31:52 +0300

    Amicable Numbers
    Amicable Numbers

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFriend = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFriend
+            // 
+            this.btnFriend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFriend.Location = new System.Drawing.Point(12, 191);
+            this.btnFriend.Name = "btnFriend";
+            this.btnFriend.Size = new System.Drawing.Size(113, 41);
+            this.btnFriend.TabIndex = 0;
+            this.btnFriend.Text = "Arkadaş Mı";
+            this.btnFriend.UseVisualStyleBackColor = true;
+            this.btnFriend.Click += new System.EventHandler(this.btnFriend_Click);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFinish.Location = new System.Drawing.Point(154, 191);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(103, 41);
+            this.btnFinish.TabIndex = 1;
+            this.btnFinish.Text = "SON";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 340);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnFriend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFriend;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,138 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2014-2015 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: Ödev 2
+**				ÖĞRENCİ ADI............: Muhammed Efecan PINAR
+**				ÖĞRENCİ NUMARASI.......: G181210092
+**              DERSİN ALINDIĞI GRUP...: 2D
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        // Return friend number
+        public IEnumerable<int> calculate(int number)
+        {
+            List<int> Arr = new List<int>();
+            for(int i = 1; i < number; i++)
+            {
+                if(number%i == 0)
+                {
+                    Arr.Add(i);
+                }
+            }
+
+            return Arr;
+        }
+
+
+        private void AddTools(IEnumerable<int> multiOfX, IEnumerable<int> multiOfY)
+        {
+            // Form
+            this.Height = 400;
+            this.Width = 800;
+
+            // TextBox
+            TextBox txtXTotal = new TextBox();
+            txtXTotal.Width = 100;
+            txtXTotal.Location = new Point(400, 160);
+
+            TextBox txtYTotal = new TextBox();
+            txtYTotal.Width = 100;
+            txtYTotal.Location = new Point(600, 160);
+
+            txtXTotal.Text = multiOfX.Sum().ToString();
+            txtYTotal.Text = multiOfY.Sum().ToString();
+
+            // Label
+            Label labelListX = new Label();
+            labelListX.Width = 200;
+            labelListX.Text = "X";
+            labelListX.Location = new Point(440, 35);
+
+            Label labelListY = new Label();
+            labelListY.Text = "Y";
+            labelListY.Width = 200;
+            labelListY.Location = new Point(640, 35);
+
+            Label labelTotal = new Label();
+            labelTotal.Width = 200;
+            labelTotal.Text = "Toplam: ";
+            labelTotal.Location = new Point(350, 164);
+
+            // ListBox
+            ListBox ListBoxX = new ListBox();
+            ListBoxX.Width = 100;
+            ListBoxX.Height = 100;
+            ListBoxX.Location = new Point(400, 60);
+
+            ListBox ListBoxY = new ListBox();
+            ListBoxY.Width = 100;
+            ListBoxY.Height = 100;
+            ListBoxY.Location = new Point(600, 60);
+
+            ListBoxX.DataSource = multiOfX;
+            ListBoxY.DataSource = multiOfY;
+
+            // Adding toolbox to the form
+            this.Controls.Add(txtXTotal);
+            this.Controls.Add(txtYTotal);
+            this.Controls.Add(labelListX);
+            this.Controls.Add(labelListY);
+            this.Controls.Add(ListBoxX);
+            this.Controls.Add(ListBoxY);
+            this.Controls.Add(labelTotal);
+
+        }
+        // Is it friend button
+        private void btnFriend_Click(object sender, EventArgs e)
+        {
+            IEnumerable<int> multiOfX;
+            IEnumerable<int> multiOfY;
+
+            try
+            {
+                multiOfY = calculate(Convert.ToInt32(textBox2.Text));
+                multiOfX = calculate(Convert.ToInt32(textBox1.Text));
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            AddTools(multiOfX, multiOfY);
+
+            Button btnFriend = (Button)sender;
+            btnFriend.Enabled = false;
+        }
+        // Finish button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
